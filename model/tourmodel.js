8a7841c5869013a92293a97989421be2d3cbf5b7
Adityasinghboranaekikrit <aditya.singh@ekikrit.com>
2024-03-22 14:04:03 +0530

    changed home page schema

--- model/tourmodel.js
+++ model/tourmodel.js
@@ -1,52 +1,40 @@
-
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const tourmodel = {
+  async getallCity() {
+    return await prisma.City.findMany();
+  },
+  async CityTourTypes() {
+    const uniqueCityTourTypes = await prisma.tourstaticdata.findMany({
+      distinct: ["cityTourType"],
+      select: {
+        cityTourType: true,
+      },
+    });
+    return uniqueCityTourTypes;
+  },
 
-const tourmodel  = {
+  async getalltours() {
+    return await prisma.tourstaticdata.findMany({
+      include: {
+        tourdetails: true,
+      },
+    });
+  },
 
-async getallCity (){
-   return await prisma.City.findMany();
-},
-async CityTourTypes() {
-   
-        const uniqueCityTourTypes = await prisma.tourstaticdata.findMany({
-        distinct: ['cityTourType'],
-          select: {
-            cityTourType: true, 
-          },
-        });
-        return uniqueCityTourTypes;
-      }, 
-  
-async getalltours (){
-    return await prisma.tourstaticdata.findMany(
-      {
-    
-        include: {
-         
-        tourdetails:true,
-        },
-      }
-    );
- },
-
-
- // not using this function 
- async gettourdata (tourId){
-  return await prisma.tourstaticdatabyid.findUnique({
-    where:{
-      id:tourId
-    },
-    include: {
-     
-    tourImages:true,
-    tourReviews:true,
-    },
-  });
-}
-// end of the function get tor data 
-
-
-}
+  // not using this function
+  async gettourdata(tourId) {
+    return await prisma.tourstaticdatabyid.findUnique({
+      where: {
+        id: tourId,
+      },
+      include: {
+        tourImages: true,
+        tourReviews: true,
+      },
+    });
+  },
+  // end of the function get tor data
+};
 module.exports = tourmodel;
